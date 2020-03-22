0226cc56cc37d5eeb0f07dcc6f5917909bd335e8
Windholz Bernd <Bernd.Windholz@ait.ac.at>
2020-03-22 09:02:43 +0100

    bring data into form

--- Exercise 0/dataSetAnalysis.py
+++ Exercise 0/dataSetAnalysis.py
@@ -1,3 +1,6 @@
-import arff
-data = arff.load(open('c:\\git\\MachineLearning2020S\\MachineLearning2020S\\Exercise 0\\communities_data\\communities.arff', 'rb'))
-print(data)
+import arff, pandas as pd
+data = arff.load(open('c:\\git\\MachineLearning2020S\\MachineLearning2020S\\Exercise 0\\communities_data\\communities.arff', 'r'))
+print(data['attributes'])
+# df = pd.from_dataframe(data)
+for key in data.keys:
+    print(key)
