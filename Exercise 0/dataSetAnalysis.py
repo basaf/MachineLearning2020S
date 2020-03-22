043dd805a3f5c91b3cb4e5468f7e8a215eb7ef36
Windholz Bernd <Bernd.Windholz@ait.ac.at>
2020-03-22 10:08:38 +0100

    improved communities histogram

--- Exercise 0/dataSetAnalysis.py
+++ Exercise 0/dataSetAnalysis.py
@@ -1,14 +1,15 @@
 from arff import load
 from pandas import DataFrame
-# import sklearn
 import matplotlib.pyplot as plt 
 
+# load dataset (.arff) into pandas DataFrame
 data = load(open('c:\\git\\MachineLearning2020S\\MachineLearning2020S\\Exercise 0\\communities_data\\communities.arff', 'r'))
 attributes = list(i[0] for i in data['attributes'])
-
 df = DataFrame(columns=attributes, data=data['data'])
-print(df.describe())
+# print(df.describe())
 
-histogram = df.hist(figsize=(150, 200))
+# plot and save histogram
+histogram = df.hist(figsize=(27, 36))
 plt.savefig('communities_data_histogram.png')
+
 print('Fertig')
