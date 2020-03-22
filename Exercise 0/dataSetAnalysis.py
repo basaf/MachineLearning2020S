b48181977f4feaaaa6b6e56c366fbf9d72755f32
Windholz Bernd <Bernd.Windholz@ait.ac.at>
2020-03-22 09:24:34 +0100

    communities dataset in DataFrame

--- Exercise 0/dataSetAnalysis.py
+++ Exercise 0/dataSetAnalysis.py
@@ -1,6 +1,7 @@
-import arff, pandas as pd
-data = arff.load(open('c:\\git\\MachineLearning2020S\\MachineLearning2020S\\Exercise 0\\communities_data\\communities.arff', 'r'))
-print(data['attributes'])
-# df = pd.from_dataframe(data)
-for key in data.keys:
-    print(key)
+from arff import load
+from pandas import DataFrame
+data = load(open('c:\\git\\MachineLearning2020S\\MachineLearning2020S\\Exercise 0\\communities_data\\communities.arff', 'r'))
+attributes = list(i[0] for i in data['attributes'])
+
+df = DataFrame(columns=attributes, data=data['data'])
+print(df)
