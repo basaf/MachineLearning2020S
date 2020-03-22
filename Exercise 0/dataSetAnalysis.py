ca06760eb10589cf34fb0b6e4177d0f991021653
Windholz Bernd <Bernd.Windholz@ait.ac.at>
2020-03-22 14:02:43 +0100

    histograms divided in predictive and goal attributes, not predictive attributes dismissed

--- Exercise 0/dataSetAnalysis.py
+++ Exercise 0/dataSetAnalysis.py
@@ -4,13 +4,34 @@ import matplotlib.pyplot as plt
 
 # load dataset (.arff) into pandas DataFrame
 data = load(open('c:\\git\\MachineLearning2020S\\MachineLearning2020S\\Exercise 0\\communities_data\\communities.arff', 'r'))
-attributes = list(i[0] for i in data['attributes'])
-df = DataFrame(columns=attributes, data=data['data'])
-# print(df.describe())
+all_attributes = list(i[0] for i in data['attributes'])
+df = DataFrame(columns=all_attributes, data=data['data'])
 
-# plot and save histogram
-hist = df.hist(figsize=(27, 36))
+# divide attributes in not_predictive, predictive and goal
+not_predictive_attributes = [
+    'state',
+    'county',
+    'community',
+    'communityname',
+    'fold'
+    ]
+goal_attribute = 'ViolentCrimesPerPop'
+
+predictive_attributes = all_attributes.copy()
+predictive_attributes.remove(goal_attribute)
+for x in not_predictive_attributes:
+    predictive_attributes.remove(x)
+
+
+# plot and save histograms
+hist = df[predictive_attributes].hist(figsize=(27, 36))
 plt.tight_layout()
-plt.savefig('images\\communities_data_histogram.png')
+plt.savefig('images\\communities_data_histogram_predictive.png')
+plt.close()
+
+hist = df[goal_attribute].hist()
+plt.tight_layout()
+plt.savefig('images\\communities_data_histogram_goal.png')
+plt.close()
 
 print('Fertig')
