431ab87d8d73d3d3f532a196c7c2eaba322b2076
Gernot <gernot.steindl@tuwien.ac.at>
2020-04-23 08:16:48 +0200

    Add "traffic volume" data set

---
+++ Exercise 1/trafficVolume.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 08:07:04 2020
+
+@author: guser
+"""
+import pandas as pd
+
+dataSetPath='./DataSets/Metro_Interstate_Traffic_Volume.csv'
+
+rawData=pd.read_csv(dataSetPath)
