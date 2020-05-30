3027f903cf55c1a849cd015d3c15fe610c4a4a71
Jürgen Pannosch <juergen.pannosch@tuwien.ac.at>
2020-05-30 08:22:01 +0200

    added - at amazon and congressional voting added a check if there are empty cells in the data

--- Exercise 2/amazon.py
+++ Exercise 2/amazon.py
@@ -12,7 +12,15 @@ training_data = pd.read_csv(os.path.join(cfg.default.amazon_data, 'amazon_review
 test_data = pd.read_csv(os.path.join(cfg.default.amazon_data, 'amazon_review_ID.shuf.tes.csv'),
                         sep=',', header=0, index_col=0)
 
-training_data_x = training_data.loc[:, training_data.columns != 'Class']
-training_data_y = training_data.loc[:, 'Class']
+# check if in the training data are empty cells
+number_of_empty_cells = training_data.isnull().sum().sum()
+
+if number_of_empty_cells > 0:
+    # handle empty cells
+    # TODO: implement strategy
+    print(f'Number of empty cells {number_of_empty_cells}')
+
+training_data_x = training_data.loc[:, training_data.columns != 'Class']  # input for classification
+training_data_y = training_data.loc[:, 'Class']  # output class
 
 print('ende')
--- Exercise 2/congressionalVoting.py
+++ Exercise 2/congressionalVoting.py
@@ -12,6 +12,14 @@ training_data = pd.read_csv(os.path.join(cfg.default.congressional_voting_data, 
 test_data = pd.read_csv(os.path.join(cfg.default.congressional_voting_data, 'CongressionalVotingID.shuf.test.csv'),
                         sep=',', header=0, index_col=0)
 
+# check if in the training data are empty cells
+number_of_empty_cells = training_data.isnull().sum().sum()
+
+if number_of_empty_cells > 0:
+    # handle empty cells
+    # TODO: implement strategy
+    print(f'Number of empty cells {number_of_empty_cells}')
+
 training_data_x = training_data.loc[:, training_data.columns != 'class']
 training_data_y = training_data.loc[:, 'class']
 
